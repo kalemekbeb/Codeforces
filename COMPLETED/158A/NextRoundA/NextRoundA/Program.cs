28380672e3ec8c7ea5928bb5968d55033ff0415f
kalemekbeb <190982582+kalemekbeb@users.noreply.github.com>
2026-04-24 11:58:52 -0400

    completed problem 158A

---
+++ COMPLETED/158A/NextRoundA/NextRoundA/Program.cs
@@ -0,0 +1,22 @@
+﻿string kn = Console.ReadLine();
+string[] nums = kn.Split();
+int n = int.Parse(nums[0]);
+int k = int.Parse(nums[1]);
+
+string scores = Console.ReadLine();
+string[] scoreArr = scores.Split();
+
+int scoreLessThanK = (int.Parse(scoreArr[k - 1]));
+int scoreCount = 0;
+for (var i=0; i<scoreArr.Length; i++)
+{
+    int score = int.Parse(scoreArr[i]);
+    if (score >= scoreLessThanK && score > 0)
+    {
+        scoreCount++;
+    }
+}
+Console.WriteLine(scoreCount);
+
+
+
