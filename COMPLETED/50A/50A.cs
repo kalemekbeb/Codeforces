f6692f0417e931f4a89054a2b8df74da38bf448b
kalemekbeb <190982582+kalemekbeb@users.noreply.github.com>
2026-04-25 14:21:51 -0400

    Completed problem 50A

---
+++ COMPLETED/50A/50A.cs
@@ -0,0 +1,7 @@
+﻿string rectInput = Console.ReadLine();
+string[] rectangle = rectInput.Split();
+int M = int.Parse(rectangle[0]);
+int N = int.Parse(rectangle[1]);
+
+int output = M * N / 2;
+Console.WriteLine(output);
