0bff1ac4650e255715483e055ed709d57eb875ee
kalemekbeb <190982582+kalemekbeb@users.noreply.github.com>
2026-04-27 13:28:51 -0400

    263A - Complete

---
+++ COMPLETED/263A/263A.cs
@@ -0,0 +1,27 @@
+﻿int[,] grid = new int[5, 5];
+
+for (int i = 0; i < 5; i++)
+{
+    string[] parts = Console.ReadLine().Split();
+    for (int j = 0; j < 5; j++)
+    {
+        grid[i, j] = int.Parse(parts[j]);
+    }
+}
+
+int oneRow = 0;
+int oneCol = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (grid[i, j] == 1)
+        {
+            oneRow = i;
+            oneCol = j;
+        }
+    }
+}
+
+Console.WriteLine(Math.Abs(oneRow - 2) + Math.Abs(oneCol - 2));
