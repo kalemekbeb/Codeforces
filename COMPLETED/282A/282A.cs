8df2bfd3bc80f81786b6badf0120386a734d69c4
kalemekbeb <190982582+kalemekbeb@users.noreply.github.com>
2026-05-05 16:48:35 -0400

    Completed problem 282A

---
+++ COMPLETED/282A/282A.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+int x = 0;
+for (int i = 0; i < n; i++)
+{
+    string statement = Console.ReadLine();
+    if (statement.Contains("++"))
+
+    {
+        x++;
+    }
+    else if (statement.Contains("--"))
+    {
+        x--;
+    }
+
+}
+Console.WriteLine(x);
